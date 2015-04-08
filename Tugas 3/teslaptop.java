eb2bf74a071eb690873c8c7320c40af45fde92c8
Muhammad Irfan Awaluddien <s_jos@ymail.com>
2015-04-08 17:44:08 +0700

    UpDate - Tugas Ketiga

---
+++ Tugas 3/teslaptop.java
@@ -0,0 +1,13 @@
+class teslaptop
+{
+	public static void main(String[] args)
+	{
+//instansiasi objek
+//namaClass namaObjek = namaKonstruktor
+	laptop bolank = new laptop();
+
+	bolank.setWarna("hitam");
+	
+	System.out.println("Warna="+ bolank.getWarna());
+	}
+}
